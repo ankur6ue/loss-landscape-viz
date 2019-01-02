8d90cea3ad934a025ec6927e79c8f51a11700cff
ankur6ue <ankur6ue@gmail.com>
2019-01-02 09:36:10 -0500

    implemented picking
    1. added mouse picking
    2. wireframe rendering
    3. multiple models
    4. slider for changing light intensity

--- client/loss_viz.js
+++ client/loss_viz.js
@@ -2,7 +2,50 @@
 var scene;
 var renderer;
 var camera;
+var light;
+var raycaster;
+var mouse;
 
+function onMouseMove(event) {
+
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	var bounds = event.target.getBoundingClientRect();
+	var x = event.clientX - bounds.left;
+	var y = event.clientY - bounds.top;
+	$("#tooltip").text("");
+	$( "#tooltip" ).position({
+		my: "left+3 bottom-3",
+		of: event,
+		collision: "fit"
+	});
+	mouse.x = (x / renderer.domElement.clientWidth ) * 2 - 1;
+    mouse.y = -(y / renderer.domElement.clientHeight ) * 2 + 1;
+	//console.log('x = '+ mouse.x + ' y = ' + mouse.y )
+	raycaster.setFromCamera(mouse, camera);
+
+	// calculate objects intersecting the picking ray
+	var intersects = raycaster.intersectObjects(scene.children);
+	if (intersects.length > 0) {
+		var x = intersects[0].point.x
+		var y = intersects[0].point.y
+		var z = intersects[0].point.z
+		console.log('x = '+ x + ' y = ' + y + ' z = ' + z)
+		$("#tooltip").text('x = '+ parseFloat(x).toFixed(2) + ' y = ' + parseFloat(y).toFixed(2) + ' z = ' + parseFloat(z).toFixed(2));
+	}
+}
+
+function onMouseDown(event) {
+	// update the picking ray with the camera and mouse position
+	var bounds = event.target.getBoundingClientRect();
+	var x = event.clientX - bounds.left;
+	var y = event.clientY - bounds.top;
+	mouse.x = (x / renderer.domElement.clientWidth ) * 2 - 1;
+    mouse.y = -(y / renderer.domElement.clientHeight ) * 2 + 1;
+	
+
+}
 function create_mesh(X, Y, Z) {
 	var n = X.length,
 	m = X.length;
@@ -44,27 +87,25 @@ function create_mesh(X, Y, Z) {
 		var zmid = 0.5 * (zmin + zmax);
 	var zrange = zmax - zmin;
 
-	for (i = 0; i < n; i++) {
-		if (values[i] > zmax)
-			values[i] = zmax
-	}
+	for (var i = 0; i < nverts; i++)
+		values[i] = values[i] > 20 ? 20 : values[i]
 
-	var scalefac = 1 //1.2/Math.max(xrange, yrange);
-		var scalefacz = 0.1 //0.5/zrange;
+			var scalefac = 1 //1.2/Math.max(xrange, yrange);
+			var scalefacz = 0.1 //0.5/zrange;
 
-		// Use d3 for color scale
-		var color = d3.scaleLinear()
-		.domain(d3.extent(values))
-		.interpolate(function () {
-			return d3.interpolateRdBu;
-		});
+			// Use d3 for color scale
+			var color = d3.scaleLinear()
+			.domain(d3.extent(values))
+			.interpolate(function () {
+				return d3.interpolateRdBu;
+			});
 
 	// Initialise threejs geometry
 	var geometry = new THREE.Geometry();
 
 	// Add grid vertices to geometry
 	for (var k = 0; k < nverts; ++k) {
-		var newvert = new THREE.Vector3((xgrid[k] - xmid) * scalefac, (ygrid[k] - ymid) * scalefac, (values[k] - zmid) * scalefacz);
+		var newvert = new THREE.Vector3((xgrid[k] - xmid) * scalefac, (ygrid[k] - ymid) * scalefac, (values[k]) * scalefacz);
 		geometry.vertices.push(newvert);
 	}
 
@@ -93,13 +134,25 @@ function create_mesh(X, Y, Z) {
 	geometry.computeVertexNormals();
 
 	// Use MeshPhongMaterial for a reflective surface
+	/*
+	var material = new THREE.MeshPhongMaterial({
+	side: THREE.DoubleSide,
+	color: 0xffffff,
+	vertexColors: THREE.VertexColors,
+	specular: 0x050505,
+	shininess: 100.,
+	emissive: 0x111111,
+	});
+	 */
 	var material = new THREE.MeshPhongMaterial({
 			side: THREE.DoubleSide,
 			color: 0xffffff,
 			vertexColors: THREE.VertexColors,
-			specular: 0x050505,
-			shininess: 100.,
-			emissive: 0x111111,
+			specular: 0,
+			shininess: 0.0,
+			emissive: 0,
+			opacity: 1,
+			wireframe: false
 		});
 	return new THREE.Mesh(geometry, material)
 }
@@ -116,16 +169,21 @@ function init(mesh) {
 			alpha: true,
 			antialias: true
 		});
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(900, 900);
+	renderer.setPixelRatio(1);
+	renderer.setSize(800, 800);
 
 	// Set target DIV for rendering
-	var container = document.getElementById('plot');
+	var container = document.getElementById('canvas');
 	container.appendChild(renderer.domElement);
 
 	// Define the camera
-	camera = new THREE.PerspectiveCamera(45, 1, 0.1, 10);
-	camera.position.z = 4;
+	camera = new THREE.PerspectiveCamera(45, 1, 0.1, 40);
+	camera.position.z = 5;
+	camera.position.y = 4;
+
+	// for picking
+	raycaster = new THREE.Raycaster();
+	mouse = new THREE.Vector2();
 
 	// Add controls
 	var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -134,19 +192,22 @@ function init(mesh) {
 	});
 	controls.enableZoom = true;
 
+	document.addEventListener('mousemove', onMouseMove, false);
+	//document.addEventListener('mousedown', onMouseDown, false);
+
 	// Light above
-	var light = new THREE.PointLight(0xffffff);
+	light = new THREE.PointLight(0xffffff);
 	light.position.set(0, 0, 3);
 	scene.add(light);
 
 	// Light below
-	var light = new THREE.PointLight(0xffffff);
-	light.position.set(0, 0, -3);
-	scene.add(light);
+	var _light = new THREE.PointLight(0xffffff);
+	_light.position.set(0, 0, -5);
+	scene.add(_light);
 
 	// Ambient light
-	var light = new THREE.AmbientLight(0x222222);
-	scene.add(light);
+	var _light = new THREE.AmbientLight(0x222222);
+	scene.add(_light);
 
 	// Make initial call to render scene
 	renderer.render(scene, camera);
@@ -156,18 +217,51 @@ function add_mesh(mesh) {
 	scene.add(mesh);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 	var this1 = this;
-	$('.ModelDropDownListItem').click(function(e) {
+	$("#slider1").slider({
+		max: 4,
+		step: 0.2,
+		slide: function (event, ui) {
+			this1.light.intensity = ui.value
+				this1.renderer.render(this1.scene, this1.camera);
+		}
+	});
+
+	$('#wireframe').change(function () {
+		if ($(this).prop('checked')) {
+			for (model in this1.models) {
+				this1.models[model].mesh.material.wireframe = true;
+			}
+		} // enable wireframe on all models
+		else {
+			for (model in this1.models) {
+				this1.models[model].mesh.material.wireframe = false;
+			}
+		}
+		this1.renderer.render(this1.scene, this1.camera);
+	})
+
+	$('.ModelDropDownListItem').click(function (e) {
 		var name = e.currentTarget;
 		var modelName = name.getAttribute("data-name")
-		console.log(modelName);
-		for (model in this1.models){
-			this1.models[model].mesh.visible = false;
+			console.log(modelName);
+		// hide all models and then make the selected one visible, unless 'all' is selected, in which case make
+		// all of them visible
+		if (modelName == 'all') {
+			for (model in this1.models)
+				this1.models[model].mesh.visible = true;
+
+		} else {
+			for (model in this1.models) {
+				this1.models[model].mesh.visible = false;
+			}
+
+			this1.models[modelName].mesh.visible = true
 		}
-		this1.models[modelName].mesh.visible = true	
 		this1.activeModel = modelName;
 		this1.renderer.render(this1.scene, this1.camera);
 	});
-	
-	}.bind(this), false);
+
+}
+	.bind(this), false);
